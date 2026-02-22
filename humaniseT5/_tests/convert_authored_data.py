71f29fabdfa20c5f0b2e1244e11582b2f0a3a39b
Korok <83775716+Kor-ok@users.noreply.github.com>
2026-02-22 10:22:26 +0000

    minor

--- humaniseT5/_tests/convert_authored_data.py
+++ humaniseT5/_tests/convert_authored_data.py
@@ -59,29 +59,29 @@ COLOUR_CODING = {
     bool: Fore.MAGENTA,
     }
 
-def _type_parser(input_value: Any) -> str:
+def values_aligner(keys: list[str], padding: int = 0) -> int:
+     """Helper to get the max of line lengths from the list of keys"""
+     return max(len(key) for key in keys) + padding
+
+def _type_parser(input_value: Any) -> tuple[str, str]:
     """Helper function to format the type of an input value for display in test output."""
     type_args = get_args(input_value.__orig_class__) if hasattr(input_value, "__orig_class__") else type(input_value)
     # If an element from the type tuple appears in our colour coding, use the corresponding colour; otherwise, default to white.
     for type_arg in (type_args if isinstance(type_args, tuple) else (type_args,)):
         colour = COLOUR_CODING.get(type_arg, Fore.WHITE)
         break
-    return f"{Fore.RESET}{input_value!r} {colour}{type_args}{Style.RESET_ALL}"
-
-def values_aligner(keys: list[str], padding: int = 0) -> int:
-     """Helper to get the max of line lengths from the list of keys"""
-     return max(len(key) for key in keys) + padding
-
+    return f"{Fore.RESET}{input_value.value}", f"{colour}{type_args}{Style.RESET_ALL}"
 
 converters: ConvertersType = {
     "single int": Converters.to_python_int,
     "comma delimited int list": Converters.list_int,
     "simple string": Converters.to_str,
     "comma delimited string list": Converters.list_str,
-    "tuple[int, …]": Converters.tuple_ints,
-    "nested tuple[Any, …]": Converters.tuple_ints,
+    "tuple[int, …]": Converters.tuple_int,
+    "nested tuple[Any, …]": Converters.tuple_int,
     "bool true": Converters.to_python_bool,
     "bool false": Converters.to_python_bool,
+    # "empty": None,
     # "code text": None,
 }
 
@@ -104,22 +104,14 @@ def debug_display() -> None:
     def test_fake_authored_data_cases() -> None:
         alignment_padding = values_aligner(list(result.keys()))
         for case_name, input_value in result.items():
-            print(f"{Fore.CYAN}{case_name}: {(' ' * (alignment_padding - len(case_name)))}{_type_parser(input_value)}{Style.RESET_ALL}")
+            type_str, type_coloured = _type_parser(input_value)
+            print(f"{Fore.CYAN}{case_name}: {(' ' * (alignment_padding - len(case_name)))}{type_str} {type_coloured}{Style.RESET_ALL}")
 
     def test_converted_values() -> None:
         for case_name, input_value in result.items():
-            alignment_padding = values_aligner(list(result.keys()))
-            print(f"{Fore.YELLOW}{case_name}: {(' ' * (alignment_padding - len(case_name)))}{_type_parser(input_value)}{Style.RESET_ALL}")
-            value = input_value.value if hasattr(input_value, "value") else f"{Fore.RED}?{input_value}{Style.RESET_ALL}"
-            print(f"{Fore.BLUE}    Extracted: {value}{Style.RESET_ALL}")
-            # convert_authored_data_via_ast(input_value)
-            # try:
-            #     converted, type_arg = convert_authored_data_via_ast(input_value)
-            # except Exception as e:
-            #     print(f"{Fore.RED}Error converting {case_name}: {e}{Style.RESET_ALL}")
-            #     continue
-            # alignment_padding = values_aligner(list(result.keys()))
-            # print(f"{Fore.MAGENTA}Converted {case_name}: {(' ' * (alignment_padding - len(case_name)))}{_type_parser(converted)} {Fore.BLUE}({type_arg}){Style.RESET_ALL}")
+            keys_alignment_padding = values_aligner(list(result.keys()))
+            value_str, type_str = _type_parser(input_value)
+            print(f"{Fore.YELLOW}{case_name}: {(' ' * (keys_alignment_padding - len(case_name)))}{value_str} {type_str}{Style.RESET_ALL}")
 
     # test_fake_authored_data_cases()
     # print()
